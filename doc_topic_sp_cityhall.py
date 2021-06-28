79fc87886a69b6a6f681faafb4993c2e0d57f37c
Guilherme Giuliano Nicolau <79374447+ggnicolau@users.noreply.github.com>
2021-06-28 19:25:26 -0300

    Create doc_topic_sp_cityhall.py

---
+++ doc_topic_sp_cityhall.py
@@ -0,0 +1,14 @@
+
+
+
+
+#%%----------------------------------------------------------------
+#lINKS
+https://stackoverflow.com/questions/53313575/how-to-get-document-topics-distribution-of-all-of-the-document-in-gensim-lda
+https://stackoverflow.com/questions/43357247/get-document-topics-and-get-term-topics-in-gensim
+https://stackoverflow.com/questions/51157395/get-document-topics-returns-empty-list-of-topics
+https://stackoverflow.com/questions/45145368/python-scikit-learn-get-documents-per-topic-in-lda
+
+
+
+
